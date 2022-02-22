bd36d62a33d1af26292f0caf039ac81427f32fb1
Arshdeep Singh <arshdeep0220@gmail.com>
2022-02-22 17:48:20 +0530

    FirstCommit

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express= require("express");
 
 const app = express();
 
+app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-    res.send("hello my name is Arshdeep Singh");
+    res.render("home");
 })
 
 app.listen(3000,function()
