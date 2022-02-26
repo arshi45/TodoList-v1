0d6fa83e86c4046c99b4945cfb0c14069084b888
Arshdeep Singh <arshdeep0220@gmail.com>
2022-02-26 22:11:44 +0530

    Base

--- app.js
+++ app.js
@@ -1,12 +1,109 @@
 const express= require("express");
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
-
+const _ = require("lodash");
+const { redirect } = require("express/lib/response");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set('view engine','ejs');
 
+mongoose.connect('mongodb+srv://admin-arshdeep:arsh123@cluster0.wkoqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+const itemSchema = {
+    name: String
+}
+const Item = mongoose.model("Item",itemSchema);
+
+const listSchema = {
+    name: String,
+    tasks : [itemSchema]
+}
+const List = mongoose.model("List",listSchema);
+
 app.get('/',function(req,res){
-    res.render("home");
-})
+
+    Item.find({},function(err,result)
+    {
+        if( result.length === 0)
+        {
+            res.render("home",{listTitle:"Today",tasks:[{name:"Empty List"}]});
+        }
+        else{
+            res.render("home",{listTitle:"Today",tasks:result});
+        }
+    });
+});
+
+app.post("/",function(req,res){
+    const newTask = req.body.task;
+    const list = req.body.list;
+
+    const item = new Item({
+        name : newTask
+    });
+
+    if(list === "Today"){
+        item.save();
+        res.redirect("/");
+    }
+    else{
+        List.findOne({name:list},function(err,results)
+        {
+            if(results){
+                results.tasks.push(item);
+                results.save();
+                res.redirect("/"+list);
+            }
+        });
+    }
+});
+
+app.post("/delete",function(req,res){
+    var itemID = req.body.checkbox;
+    var listName = req.body.listName;
+
+    if(listName === "Today")
+    {
+        Item.findByIdAndRemove(itemID,function(err)
+        {
+            if(!err)
+            res.redirect("/");
+        });
+    }
+    else
+    {
+        List.findOneAndUpdate(
+            {name:listName},
+            {$pull:{tasks:{_id:itemID}}},
+            function(err)
+            {
+                if(!err)
+                res.redirect("/"+listName);
+            }
+        );
+    }
+});
+
+app.get("/:customName",function(req,res){
+    var customListName = _.capitalize(req.params.customName);
+
+    List.findOne({name:customListName},
+        function(err,result)
+    {
+        if(!result){
+            const customName = new List({
+                name: customListName,
+            });
+            customName.save();
+            res.redirect("/"+customListName);
+        }
+        else{
+            res.render("home",{listTitle:result.name,tasks:result.tasks})
+        }
+    });
+
+});
 
 app.listen(3000,function()
 {
